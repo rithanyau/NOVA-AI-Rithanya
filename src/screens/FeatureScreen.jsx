55b1756e571ab62718e0bc4fab07a257cfbadcfc
Rithanya Udhayakumar <rithanyau88@gmail.com>
2026-05-29 06:33:22 +0530

    Fixed backend route

--- src/screens/FeatureScreen.jsx
+++ src/screens/FeatureScreen.jsx
@@ -27,7 +27,7 @@ function FeatureScreen({ feature, setScreen }) {
     setIsLoading(true)
 
     try {
-      const response = await fetch("https://nova-ai-rithanya-backend.onrender.com", {
+     const response = await fetch("https://nova-ai-rithanya-backend.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
